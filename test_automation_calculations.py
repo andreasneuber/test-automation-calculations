a6de32a72f0b98c177f5abe1246cf4175af74ec4
Andreas Neuber <github@andy-neuber.de>
2025-02-01 16:14:25 +0100

    Improvements

--- test_automation_calculations.py
+++ test_automation_calculations.py
@@ -1,3 +1,4 @@
+import math
 import streamlit as st
 import matplotlib.pyplot as plt
 from utils.date_utils import generate_next_6_months
@@ -14,20 +15,24 @@ st.subheader('How many work hours can be saved by automating all tests of Test S
 manual_test_execution_time = st.number_input('Manual Test Execution Time of Test Suite (in hours):', min_value=0)
 automated_test_execution_time = st.number_input('Automated Test Execution Time of Test Suite (in minutes):', min_value=0)
 automation_development_time = st.number_input('Automation Development Time (in hours):', min_value=0)
-number_execution_automation = st.number_input('Additional runs of Automated Tests after implementation:', min_value=1)
 
 # Calculate the hours saved
 hours_saved_first_run = manual_test_execution_time - ((automated_test_execution_time / 60) + automation_development_time)
-hours_saved_next_runs = (manual_test_execution_time - (automated_test_execution_time / 60)) * number_execution_automation
+time_savings_per_run = manual_test_execution_time - (automated_test_execution_time / 60)
 
 # Display the result
 if hours_saved_first_run > 0:
-    st.success(f'In implementation stage and with the 1st Run, you can save approximately {hours_saved_first_run:.2f} hours with test automation.')
+    st.success(f'In implementation stage and with the very 1st run, you can save approximately {hours_saved_first_run:.2f} hours with test automation.')
+    st.success(f'Each additional run will save you about {time_savings_per_run:.2f} hours.')
 else:
-    st.warning(f'In implementation stage and with the 1st Run, you will loose approximately {hours_saved_first_run:.2f} hours.')
+    st.warning(f'In implementation stage and with the very 1st run, you will loose approximately {hours_saved_first_run:.2f} hours.')
 
-if hours_saved_next_runs > 0 and number_execution_automation > 0:
-    st.success(f'However, with {number_execution_automation} additional runs of the automation suite, you can save approximately {hours_saved_next_runs:.2f} hours.')
+    if time_savings_per_run > 0:
+        positive_number = abs(hours_saved_first_run)
+        amortization = math.ceil(positive_number / time_savings_per_run)
+
+        st.success(f'However, each additional run will save you about {time_savings_per_run:.2f} hours.')
+        st.success(f'Means, the initial time deficit is balanced out with {amortization} additional runs of the automation suite.')
 
 st.markdown("---")
 
@@ -38,7 +43,7 @@ st.subheader('Can the team "afford" the maintenance of [n] more automated tests?
 
 # Input fields
 TH = st.number_input('Monthly hours available for maintenance tasks (TH):', min_value=0)
-MT = st.number_input('Monthly hours used to maintain existing automated tests (MT):', min_value=0)
+MT = st.number_input('Monthly hours currently used to maintain existing automated tests (MT):', min_value=0)
 N = st.number_input('Total count of all current automated tests (N):', min_value=0)
 A = st.number_input('Count of new automated tests to be added next month (A):', min_value=0)
 
@@ -75,7 +80,6 @@ if N > 0:
     ax.axhline(0, color='red', linestyle='--', linewidth=0.5)
     ax.set_xlabel('Months')
     ax.set_ylabel('Potential to add more tests (P)')
-    # ax.set_title(st.write(f'6-Month Prediction of Potential (P) for Adding {A} More Tests Each Month'))
 
 
     # Display the chart

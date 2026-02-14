d3156cb04c7123247785b2b768774b26ecab622e
Andreas Neuber <github@andy-neuber.de>
2026-02-14 16:05:46 +0100

    Improve English

--- questions/home.py
+++ questions/home.py
@@ -2,5 +2,5 @@
 import streamlit as st
 
 def show():
-    st.write("Intended usage: Follow the questions under Navigation one by one,"
-             " and avoid financial losses and frustrations.")
+    st.write("Intended usage: Follow the questions under Navigation one by one " \
+    "to avoid financial losses and frustrations.")
